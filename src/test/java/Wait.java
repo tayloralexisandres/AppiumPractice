b7b80f2dc0a250c3e562d9de5b5f1ddcb557bf2b
Taylor Andres <110685254+tayloralexisandres@users.noreply.github.com>
2023-05-07 11:28:53 -0600

    first coomit

---
+++ src/test/java/Wait.java
@@ -0,0 +1,10 @@
+public class Wait {
+
+    private static void waitfor(int i) {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
