d679df94152402eb36f2fea48c969bbc31cdf31b
Taylor Andres <110685254+tayloralexisandres@users.noreply.github.com>
2023-05-09 10:42:01 -0600

    updated

---
+++ src/test/java/com/erp/testbase/WebTestBase.java
@@ -0,0 +1,35 @@
+package com.erp.testbase;
+
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public abstract class WebTestBase {
+    protected WebDriver driver;
+    @BeforeEach
+    public void init() throws MalformedURLException, InterruptedException {
+        DesiredCapabilities capabilities=new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION,"10.0");
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME,"Pixel 3");
+        capabilities.setCapability(MobileCapabilityType.BROWSER_NAME,"Chrome");
+
+        URL url=new URL("http://localhost:4723/wd/hub");
+        driver= new RemoteWebDriver(url,capabilities);
+
+
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+    }
+}
---
+++ src/test/java/com/erp/tests/EtsyChromeTest.java
@@ -0,0 +1,59 @@
+package com.erp.tests;
+
+import com.erp.testbase.WebTestBase;
+import com.erp.utils.ConfigurationReader;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import java.net.MalformedURLException;
+
+public class EtsyChromeTest extends WebTestBase {
+
+
+    @Test
+    public void etsySearchTest() throws MalformedURLException,InterruptedException {
+
+        driver.get("https://www.etsy.com");
+
+
+        //WebElement search=driver.findElement(By.id("com.android.chrome:id/search_box_text"));
+        //search.sendKeys("\"\\\"https://www.etsy.com\\\"\""+Keys.ENTER);
+
+
+        WebElement searchField=driver.findElement(By.name("search_query"));
+        searchField.sendKeys("wooden spoon"+ Keys.ENTER);
+        WebElement results=driver.findElement(By.xpath("//span[contains(text(),'results,')]"));
+        System.out.println(results.getText());
+
+//email-name
+    }
+
+    @Test
+    public void logintest() throws InterruptedException {
+
+        driver.get("https://www.etsy.com");
+
+        String email=ConfigurationReader.getProperty("email");
+        String password=ConfigurationReader.getProperty("password");
+
+
+        WebElement signIn=driver.findElement(By.xpath("//a[@href='https://www.etsy.com/signin/router?ref=hdr-signin&from_action=signin-header&from_page=https%3A%2F%2Fwww.etsy.com%2F']"));
+        signIn.click();
+
+        WebElement userName=driver.findElement(By.name("email"));
+        userName.sendKeys(email+Keys.ENTER);
+        Thread.sleep(6000);
+        WebElement pass=driver.findElement(By.name("password"));
+        pass.sendKeys(password+Keys.ENTER);
+        WebElement button=driver.findElement(By.name("submit_attempt"));
+        Thread.sleep(6000);
+       // button.click();
+
+        Thread.sleep(6000);
+    }
+
+
+
+}
