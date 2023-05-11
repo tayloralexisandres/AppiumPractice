ec9cd747e43803e72f4cc37a99606d07a694390e
Taylor Andres <110685254+tayloralexisandres@users.noreply.github.com>
2023-05-11 15:34:59 -0600

    OSCAR update

--- src/test/java/com/erp/tests/DemoTest.java
+++ src/test/java/com/erp/tests/DemoTest.java
@@ -1,6 +1,7 @@
 package com.erp.tests;
 
 import com.saucelabs.saucebindings.junit5.SauceBaseTest;
+import io.appium.java_client.MobileBy;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.TestInfo;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.By;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
@@ -46,10 +48,21 @@ public class DemoTest {
 
     @DisplayName("W3C example with JUnit5")
     @Test
-    public void w3cExampleWithJUnit5() {
+    public void w3cExampleWithJUnit5() throws InterruptedException {
         driver.navigate().to("https://www.saucedemo.com");
         Assertions.assertEquals("Swag Labs", driver.getTitle());
         System.out.println(driver.getTitle());
+
+        //driver.findElement(MobileBy.AccessibilityId("test-Username")).sendKeys("standard_user");
+        //        Thread.sleep(4000);
+        //        driver.findElement(MobileBy.AccessibilityId("test-Password")).sendKeys("secret_sauce");
+        //        Thread.sleep(4000);
+        //        driver.findElement(MobileBy.AccessibilityId("test-LOGIN")).click();
+        //        Thread.sleep(4000);
+       // driver.findElement(By.xpath("input[@data-test='username']")).sendKeys("standard_user");
+         //input[@data-test='username']
+        //input[@data-test='password']
+        // input[@data-test='login-button']
     }
 
     /**
