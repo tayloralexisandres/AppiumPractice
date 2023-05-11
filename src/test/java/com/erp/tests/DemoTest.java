dbb60b0e8a16d9e563e5e2c424af181454a22901
Taylor Andres <110685254+tayloralexisandres@users.noreply.github.com>
2023-05-11 13:14:54 -0600

    OSCAR update

---
+++ src/test/java/com/erp/tests/DemoTest.java
@@ -0,0 +1,71 @@
+package com.erp.tests;
+
+import com.saucelabs.saucebindings.junit5.SauceBaseTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.api.extension.TestWatcher;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+
+public class DemoTest {
+
+    public RemoteWebDriver driver;
+
+    /**
+     * A Test Watcher is needed to be able to get the results of a Test so that it can be sent to Sauce Labs.
+     * Note that the name is never actually used
+     */
+    @RegisterExtension
+    public SauceTestWatcher watcher = new SauceTestWatcher();
+
+    @BeforeEach
+    public void setup(TestInfo testInfo) throws MalformedURLException {
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+        sauceOptions.setCapability("username", System.getenv("SAUCE_USERNAME"));
+        sauceOptions.setCapability("access_key", System.getenv("SAUCE_ACCESS_KEY"));
+        sauceOptions.setCapability("name", testInfo.getDisplayName());
+        sauceOptions.setCapability("browserVersion", "latest");
+
+        ChromeOptions options = new ChromeOptions();
+        options.setCapability("sauce:options", sauceOptions);
+        URL url = new URL("https://ondemand.us-west-1.saucelabs.com/wd/hub");
+
+        driver = new RemoteWebDriver(url, options);
+    }
+
+    @DisplayName("W3C example with JUnit5")
+    @Test
+    public void w3cExampleWithJUnit5() {
+        driver.navigate().to("https://www.saucedemo.com");
+        Assertions.assertEquals("Swag Labs", driver.getTitle());
+        System.out.println(driver.getTitle());
+    }
+
+    /**
+     * Custom TestWatcher for Sauce Labs projects.
+     */
+   public class SauceTestWatcher implements TestWatcher {
+        @Override
+        public void testSuccessful(ExtensionContext context) {
+            driver.executeScript("sauce:job-result=passed");
+            driver.quit();
+        }
+
+        @Override
+        public void testFailed(ExtensionContext context, Throwable cause) {
+            driver.executeScript("sauce:job-result=failed");
+            driver.quit();
+        }
+    }
+}
