17ca14015a1959b1d199a1cc48c84b6385d441ab
Taylor Andres <110685254+tayloralexisandres@users.noreply.github.com>
2023-05-12 16:19:12 -0600

    ios

---
+++ src/test/java/com/erp/tests/TestiOS.java
@@ -0,0 +1,43 @@
+package com.erp.tests;
+
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.Test;
+import org.openqa.selenium.MutableCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class TestiOS {
+    private String SAUCE_US_URL = "https://ondemand.us-west-1.saucelabs.com/wd/hub";
+    private AndroidDriver driver;
+
+    @Test
+    public void test1() throws MalformedURLException {
+
+        URL url;
+        url = new URL(SAUCE_US_URL);
+
+        /// for IPHONE
+
+        MutableCapabilities capabilities = new MutableCapabilities();
+        MutableCapabilities sauceOptions = new MutableCapabilities();
+
+        capabilities.setCapability("platformName", "iOS");
+        // for ios in sacucelabs
+        capabilities.setCapability("appium:automationName", "xcuitest");
+        // call this application
+        capabilities.setCapability("appium:app",
+                "https://github.com/saucelabs/sample-app-mobile/releases/download/2.7.1/iOS.RealDevice.SauceLabs.Mobile.Sample.app.2.7.1.ipa");
+
+        // apply for the app activity
+        capabilities.setCapability("appium:appWaitActivity", "com.swaglabsmobileapp.MainActivity");
+        sauceOptions.setCapability("username", System.getenv("SAUCE_USERNAME"));
+        sauceOptions.setCapability("accessKey", System.getenv("SAUCE_ACCESS_KEY"));
+        capabilities.setCapability("sauce:options", sauceOptions);
+
+        driver = new AndroidDriver(url, capabilities);
+
+        driver.closeApp();
+
+    }
+    }
